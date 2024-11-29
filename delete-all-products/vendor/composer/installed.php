35ee255ec086c9293f07e76849fdb9441a4dff9f
Sean Murphy <sean@iamseanmurphy.com>
2024-11-29 00:35:35 +0000

    1.3.0

---
+++ delete-all-products/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '26bdc8f4c13801df9c909df9e9d75a49f670cc97',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '26bdc8f4c13801df9c909df9e9d75a49f670cc97',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
